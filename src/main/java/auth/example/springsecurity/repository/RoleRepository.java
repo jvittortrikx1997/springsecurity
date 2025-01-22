b0c4c68251309f701e6f78caf6bd17b01a545872
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2025-01-22 14:28:15 -0300

    feat: interfaces

--- src/main/java/auth/example/springsecurity/repository/RoleRepository.java
+++ src/main/java/auth/example/springsecurity/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package auth.example.springsecurity.repository;
 
-public class RoleRepository {
+import auth.example.springsecurity.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/auth/example/springsecurity/repository/UserRepository.java
+++ src/main/java/auth/example/springsecurity/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package auth.example.springsecurity.repository;
 
-public class UserRepository {
+import auth.example.springsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
